290acf1f0a196936b85d9410446c05f0d75f8afe
onyishi <vichetachi@gmail.com>
2022-09-08 22:01:27 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * return: 1 on success
- */
+ *   * main - Entry point
+ *     *
+ *       * Return: 1 on success
+ *         */
 int main(void)
 {
-	
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
-
+		printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+			return (1);
 }
