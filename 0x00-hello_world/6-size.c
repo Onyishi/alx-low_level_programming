bd762c51e3bba32ba1c828d45f2ff0bec2dd679f
onyishi <vichetachi@gmail.com>
2022-09-08 20:58:49 -0700

    ok

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,33 +1,16 @@
 #include <stdio.h>
+
 /**
- *  main - main block
- *  Return: 0
+ *  main - prints the size of various types
+ *  Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-		printf("Size of a char: %i byte(s)\n", sizeof(char));
-			printf("Size of an int: %i byte(s)\n", sizeof(int));
-				printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-						printf("Size of a float: %i byte(s)\n", sizeof(float));
-							return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
-100)#!/bin/bash
-gcc -S -masm=intel $CFILE
 
-101)#include <unistd.h>
-
-/**
- *   main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *               followed by a new line, to standard error.
- * 
- *     Return: Always 1.
- */
-int main(void)
-{
-		write(2,
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-					      	      59);
-
-			return (1);
-}
