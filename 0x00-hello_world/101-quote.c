8e3b3a688d662932a1f0df926aeca472fbda0c57
onyishi <vichetachi@gmail.com>
2022-09-08 22:04:54 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  *   * main - Entry point
  *     *
  *       * Return: 1 on success
-          */
+ 	*/
 int main(void)
 {
 		printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
