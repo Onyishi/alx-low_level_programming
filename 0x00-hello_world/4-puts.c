5f3e8f701063a5de2783917f9b6af14a78c5db6a
onyishi <vichetachi@gmail.com>
2022-09-08 20:36:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include<stdio.h>
+/** 
+ * main - print the storing in the put function
+ *
+ * Description: using in the main function
+ * this program prints 'programming is like building a multilingual puzzle
+ * Retun: 0
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return(0);
+}
+
