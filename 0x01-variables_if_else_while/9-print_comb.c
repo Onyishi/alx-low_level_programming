63630898b4b69b6eaea7a7d526c905106d6734bc
onyishi <vichetachi@gmail.com>
2022-09-09 21:38:48 -0700

    ok

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,22 +6,19 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+ */
 
 int main(void)
 {
 		int d;
 
-			for (d = '0'; d <= '9'; d++)
-					{
-								putchar(d);
-										if (d != '9')
-													{
-																	putchar(',');
-																				putchar(' ');
-																						}
-											}
-				putchar('\n');
-
-					return (0);
+		for (d = '0'; d <= '9'; d++)
+		{
+			putchar(d);
+		if (d != '9')	
+		{										putchar(',');								putchar(' ');
+                }
+		}
+		putchar('\n');
+		return (0);
 }
