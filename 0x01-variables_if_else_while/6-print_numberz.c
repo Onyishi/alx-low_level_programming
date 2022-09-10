7560d44334d45d1771749672fc4f3c56972960e0
onyishi <vichetachi@gmail.com>
2022-09-09 18:12:27 -0700

    ok

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,15 +6,15 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- */
+ *     */
 
 int main(void)
 {
 		int d;
-                        
-		for (d = '0'; d <= '9'; d++)
-			putchar(d);
-			putchar('\n');
-                                     
-			return (0);
+
+			for (d = '0'; d <= '9'; d++)
+						putchar(d);
+				putchar('\n');
+
+					return (0);
 }
