f5a5d5d46a90b2e6bbb87ab90fc36bcd779b6345
onyishi <vichetachi@gmail.com>
2022-09-09 21:45:39 -0700

    ok

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,12 +11,12 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
-		 putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+			 putchar(ch);
+		}
+		putchar('\n');
+		return (0);
 }	
 
 
