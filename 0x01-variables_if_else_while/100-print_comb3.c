f5217d954df9db51ae3aed94609253bff67ebfea
onyishi <vichetachi@gmail.com>
2022-09-12 07:56:47 -0700

    ok

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,6 +18,7 @@ int main(void)
 		i = 48;										while (i < 58)
 		{
 			if (e != i && e < i)
+
 			{											putchar(e);									putchar(i);									if (i == 57 && e == 56)
 				{
 					break;									}										putchar(',');									putchar(' ');								}
