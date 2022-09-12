a1a22b06ff9532ee31d6375b5d40bed5b8c57f2b
onyishi <vichetachi@gmail.com>
2022-09-11 22:25:02 -0700

    ok

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
+
 /**
- *  * main - printing numbers from 0-9 with commas and space between them
- *   * Description: using the main function
- *    * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
- *     * Return: 0
+ * main-Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-		int c;
+	int num;
 
-			for (c = 48; c <= 57; c++)
-					{
-								putchar(c);
-										if (c != 57)
-													{
-																	putchar(',');
-																				putchar(' ');
-																						}
-											}
-				putchar('\n');
-					return (0);
+	form(num = 0; num < 10; num++)
+		printf("%d", num);
+
+	
+	printf("\n");
+
+	return (0)
+
 }
