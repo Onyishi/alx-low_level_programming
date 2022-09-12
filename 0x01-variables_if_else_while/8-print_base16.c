378217b507ee44ceedef15931b31d52ed2b62b89
onyishi <vichetachi@gmail.com>
2022-09-11 23:13:14 -0700

    ok

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,12 +13,12 @@ int main(void)
 	int z;
 	char low;
 
-	for(z = '0'; z <= '9'; z++)
+	for (z = '0'; z <= '9'; z++)
 	{
 		putchar(z);
 	}
 
-	for(low = 'a'; low <= 'f'; low++)
+	for (low = 'a'; low <= 'f'; low++)
 	{
 		putchar(low);
 	}
