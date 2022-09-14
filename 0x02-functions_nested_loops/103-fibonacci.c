456d057f270c5b5c6aa5c94c6247a8138b851b0a
onyishi <vichetachi@gmail.com>
2022-09-13 20:38:48 -0700

    ok

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	int i = 1, j = 2, total = 0;
 	int k;
-			
-	while (j < 4000000)
+	
+	while (j < 4000000)	
 	{
 		if (j % 2 == 0)
 			total += j;
-	
 		k = j;
 		j += i;
-		i = k;									}
+		i = k;
+	}
 	printf("%d\n", total);
 	return (0);
 }
