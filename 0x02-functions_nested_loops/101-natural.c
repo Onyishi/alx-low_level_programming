067220a5016c01dc164302bf765595a0229dd4ed
onyishi <vichetachi@gmail.com>
2022-09-13 21:17:45 -0700

    ok

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,23 +11,19 @@ int main(void)
 {
 		int start_num, end_num, total;
 
-			end_num = 1024;
-				total = 0;
+		end_num = 1024;
+		total = 0;
 
-					for (start_num = 0; start_num < end_num; start_num++)
-							{
-										if ((start_num % 3 == 0) || (start_num % 5 == 0))
-													{
-																	total = total + start_num;
-																			}
-												else
-															{
-																			continue;
-																					}
-													}
+		for (start_num = 0; start_num < end_num; start_num++)
+		{
+			if ((start_num % 3 == 0) || (start_num % 5 == 0))				{
+				total = total + start_num;						}
+			else
+			{
+				continu;								}									}
 
-						printf("%d", total);
-							printf("\n");
+		printf("%d", total);
+		printf("\n");
 
-								return (0);
+		return (0);
 }
