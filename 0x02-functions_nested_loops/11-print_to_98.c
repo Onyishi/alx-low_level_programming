943103a8ca7076bfc1887350ec0418171c24661a
onyishi <vichetachi@gmail.com>
2022-09-13 19:06:58 -0700

    ok

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 {
 		if (n <= 98)
 		{
-		for (; n <= 98; n++)								{
+		for (n <= 98; n++)								{
 			printf("%d", n);							if (n =98)
 			continue;									printf(", ");
 		}
@@ -21,7 +21,7 @@ void print_to_98(int n)
 		}
 		else
 		{
-		for (; n >= 98; n--)								{											printf("%d", n);							if (n == 98)	
+		for ( n >= 98; n--)								{											printf("%d", n);							if (n == 98)	
 			continue;
 			printf(", ");								}
 			printf("\n");		
