6a7b8b68be9f419bf6c91482e76321a1e78e5468
onyishi <vichetachi@gmail.com>
2022-09-13 18:24:26 -0700

    ok

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,8 +14,15 @@ void times_table(void)
 		for (r = 0; r <= 9; r++)
 		{
 			_putchar('0');
-			_putchar(',');									_putchar(' ');
-		for (c = 1; c <= 9; c++)							{											d = (r * c);								if ((d / 10) > 0)
+			_putchar(',');
+			_putchar(' ');
+		}
+		for (c = 1; c <= 9; c++)
+
+		{
+			d = (r * c);
+		}			
+		if ((d / 10) > 0)
 		{
 			_putchar((d / 10) + '0');						}										else											{										_putchar(' ');
 													}
