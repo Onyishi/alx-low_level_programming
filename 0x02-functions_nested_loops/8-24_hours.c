2b4e3c61c9db657ce171bb36f2e6878449490c45
onyishi <vichetachi@gmail.com>
2022-09-13 17:38:39 -0700

    ok

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,15 +12,15 @@ void jack_bauer(void)
 
 	for (i = 0; i < 24; i++)
 	{
-		for (j = 0; j < 60; j++)
-		{
-			_putchar(i / 10 + 48);	
-			_putchar(i % 10 + 48);
-			_putchar(':');
-			_putchar(j / 10 + 48);
-			_putchar(j % 10 + 48);
-			_putchar('\n');
-		}
+	for (j = 0; j < 60; j++)
+	{
+		_putchar(i / 10 + 48);	
+		_putchar(i % 10 + 48);
+		_putchar(':');
+		_putchar(j / 10 + 48);
+		_putchar(j % 10 + 48);
+		_putchar('\n');
+	}
 	}
 }
 
