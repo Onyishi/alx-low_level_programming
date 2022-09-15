d94c7982d6f5cdfea9447869e6f0bf5aab115993
onyishi <vichetachi@gmail.com>
2022-09-15 06:48:59 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * file - main.h
+ * Author: Onyishi Victor Chetachi
+ * Description: Header contains prototypes for all functions
+ * 	used in 0x03-more_functions_nested_loops_directory
+ */
+
+int _putchar(char c);
+
+
+
+#endif
