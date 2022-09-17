7a03e5f1f8661650608dbe5a6810d42efd2b46df
onyishi <vichetachi@gmail.com>
2022-09-16 21:25:54 -0700

    ok

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,10 +9,10 @@ void print_line(int n)
 {
 		int i = 1;
 		
-			while (i <= n)
-			{
-				_putchar(95);
-				i++;
-			}
-			_putchar('\n');
+		while (i <= n)
+		{
+			_putchar(95);
+			i++;
+		}
+		_putchar('\n');
 }
