768862330ed6ff853a7f2aade7031638497ec86c
onyishi <vichetachi@gmail.com>
2022-10-06 22:06:40 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,32 +14,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *clone, *relloc;
 	unsigned int i;
 
-	if (ptr != NULL)
-	{
-		clone = ptr;
-	}
-	else
-	{
-		return (malloc(new_size));
-	}
-	if (new_size == old_siz)
-	{
-		return (ptr);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (0);
-	}
-	relloc = malloc(new_size);
-	if (relloc == NULL)
-	{
-		return (0);
-	}
-	for (i = 0; i < (old_size || i < new_size); i++)
-	{
-		*(relloc + i) = clone[i];
-	}
-	free(ptr);
-	return (relloc);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	;
+	return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
+	return (p);
 }
