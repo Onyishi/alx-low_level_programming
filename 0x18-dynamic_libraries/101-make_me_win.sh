805ff62ad485b958a4126a47877ba162105bd09e
onyishi <vichetachi@gmail.com>
2022-12-16 07:06:02 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -shared -o 4libc.so
-LD_PRELOAD=$PWD/4libc.so
+wget -P /tmp https://github.com/Onyishi/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so 
+export LD_PRELOAD=/tmp/hacked.so
